10ce24cadd71ef7ba70bb2325722017035eff232
timoreo <contact@timoreo.fr>
2023-09-18 11:14:17 +0200

    Delta time + fps counter

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
+from time import monotonic
 
 SCREEN_SIZE = (1280, 720)
 
 pygame.init()
-screen = pygame.display.set_mode(SCREEN_SIZE, pygame.RESIZABLE)
+screen = pygame.display.set_mode(SCREEN_SIZE, pygame.SCALED, vsync=1)
 pygame.display.set_caption('JAAJ')
 
+last_time = monotonic()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    new_time = monotonic()
+    delta = new_time - last_time
+    last_time = new_time
     screen.fill((255, 255, 255))
 
     pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
 
     pygame.display.flip()
+    print("FPS ", 1 / delta)
 
 pygame.quit()
