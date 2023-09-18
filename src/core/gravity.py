23f0ca6bc132dff28d0f38554cffcca784321f63
GaspardCulis <gasdev.lp@gmail.com>
2023-09-18 13:36:41 +0200

    Created gravity code

---
+++ src/core/gravity.py
@@ -0,0 +1,37 @@
+from pygame import Vector2
+import math
+
+G = 1.0
+
+physics_objects: list['PhysicsObject'] = []
+
+class PhysicsObject():
+    def __init__(self, mass: float, position: Vector2, passive: bool = False, static = False):
+        # La masse de l'objet
+        self.mass = mass
+        # Si l'objet a une influence gravitationnelle sur les autres.
+        self.passive = passive
+        # Si l'objet ne bouge pas
+        self.static = static
+            
+        self.position = position
+        self.velocity = Vector2(0, 0)
+
+        physics_objects.append(self)
+
+    def update_forces(self, delta: float):
+        force = Vector2(0, 0)
+        for o in filter(lambda x : not (x.passive or x == self), physics_objects):
+            distance = self.position.distance_squared_to(o.position)
+            print(o.position, self.position, distance)
+            f = G * (self.mass * o.mass) / distance
+            force += f * (o.position - self.position).normalize()
+
+        self.velocity += force / self.mass
+        self.position += self.velocity * delta
+
+
+    @staticmethod
+    def update_all(delta: float):
+        for o in filter(lambda x : not x.static, physics_objects):
+            o.update_forces(delta)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 import pygame
+from pygame import Vector2
 from time import monotonic
+from core.gravity import PhysicsObject
 
 SCREEN_SIZE = (1280, 720)
 
 pygame.init()
 screen = pygame.display.set_mode(SCREEN_SIZE, pygame.SCALED, vsync=1)
 pygame.display.set_caption('JAAJ')
+
+planet_a = PhysicsObject(69, Vector2(500, 600))
+planet_b = PhysicsObject(69, Vector2(500, 500))
+planet_c = PhysicsObject(69, Vector2(500, 550), passive=True)
+
+planet_a.velocity = Vector2(0.5, 0)
+planet_b.velocity = Vector2(-0.5, 0)
+
+planets = [
+    planet_a,
+    planet_b,
+]
 
 last_time = monotonic()
 running = True
@@ -18,7 +32,10 @@ while running:
     last_time = new_time
     screen.fill((255, 255, 255))
 
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    for p in planets:
+        pygame.draw.circle(screen, (0, 0, 255), p.position, 10)
+
+    PhysicsObject.update_all(delta*100)
 
     pygame.display.flip()
     print("FPS ", 1 / delta)
