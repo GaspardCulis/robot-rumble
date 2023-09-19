be48791144f5cafa3323151e13e37589d5bc450b
GaspardCulis <gasdev.lp@gmail.com>
2023-09-19 14:59:23 +0200

    Refactored weapon abstract class

--- src/objects/weapon.py
+++ src/objects/weapon.py
@@ -2,8 +2,14 @@ from abc import ABC, abstractmethod
 from pygame import Vector2
 
 class Weapon():
+    def __init__(self, recoil: float, cooldown_delay: float, ammo: int, reload_time: float) -> None:
+        self.recoil = recoil
+        self.cooldown_delay = cooldown_delay,
+        self.ammo = ammo,
+        self.reload_time = reload_time
+    
     @abstractmethod
-    def shoot(self, origin: Vector2, target: Vector2) -> float:
+    def shoot(self, origin: Vector2, target: Vector2):
         """
         Shoots a projectile given an origin, a target
         Returns a recoil force
