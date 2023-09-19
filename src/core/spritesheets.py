dfa82880d846d5d764cc67b7c2bf7220c49115cc
Hippolyte Chauvin <hchauvin38@outlook.fr>
2023-09-19 16:16:09 +0200

    Ajout : première version de la fonction parse_spritesheets

---
+++ src/core/spritesheets.py
@@ -0,0 +1,15 @@
+import pygame
+
+from pygame import Rect
+
+def parse_spritesheet(spritesheet: pygame.Surface, rows: int, cols: int) -> list[pygame.Surface]:
+    subsurfaces = []
+    sprite_width = spritesheet.get_width() / cols
+    sprite_height = spritesheet.get_height() / rows
+
+    for i in range(rows):
+        for j in range(cols):
+            subsurfaces.append(Rect(j * sprite_width, i * sprite_height, sprite_width, sprite_height))
+
+    return subsurfaces
+
