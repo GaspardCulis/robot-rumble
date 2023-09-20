17c916bd4b23c5acb030ac48530b5f4ac62850fe
Hippolyte Chauvin <hchauvin38@outlook.fr>
2023-09-20 16:39:59 +0200

    Ajout : son du minigun

--- src/objects/minigun.py
+++ src/objects/minigun.py
@@ -4,6 +4,8 @@ from objects.bullet import Bullet
 from core.gravity import PhysicsObject
 from objects.weapon import Weapon
 
+from core.sound import Sound
+
 class Minigun(Weapon):
     def __init__(self):
         super().__init__(30, 0.05, 50, 1.5)
@@ -11,6 +13,7 @@ class Minigun(Weapon):
     def shoot(self, position: Vector2, target: Vector2) -> Vector2:
         if self.can_shoot():
             direction = (target - position).normalize()
+            Sound.get().play('minigun')
             bullet = Bullet(position + Vector2(1, 0), direction)
             return direction * self.recoil
         return Vector2(0)
