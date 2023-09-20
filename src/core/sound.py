79cd8708c3462c9956517239dd347c83b520d3af
Hippolyte Chauvin <hchauvin38@outlook.fr>
2023-09-20 15:56:02 +0200

    Remaniement : réécrtiture de core.sound, création de la classe Sound, implémentation de méthodes de contrôle du son

--- src/core/sound.py
+++ src/core/sound.py
@@ -1,13 +1,48 @@
 from pygame import mixer
 
-def load_sound(name):
-    mixer.music.load("assets/snd/{}.ogg".format(name))
+class Sound():
+    # Dictionnaire de sons chargés
+    all: dict[str, mixer.Sound]
 
-def loop_song(name):
-    load_sound(name)
-    mixer.music.play(-1)
+    # Initialiser le mixer
+    def __init__(self):
+        mixer.init()
+        self.all = {}
 
-def stop_sound():
-    mixer.music.stop()
-    mixer.music.unload()
+    # Supprimer tous les sons de la classe et quitter le mixer
+    def __del__(self):
+        self.all.clear()
+        mixer.quit()
+
+    # Former le chemin relatif vers le fichier son auquel on souhaite accéder
+    def get_snd_path(self, name):
+        return "asset/snd/{}.ogg".format(name)
+
+    # Arrêter le morceau en cours
+    def stop_music(self):
+        if mixer.music.get_busy():
+            mixer.music.stop()
+            mixer.music.unload()
+
+    # Jouer un moceau indéfiniment
+    def loop_song(self, name):
+        self.stop_music()
+        try:
+            mixer.music.load(self.get_snd_path(name))
+            mixer.music.play(-1)
+        except FileNotFoundError:
+            pass
+
+    # Charger un son dans la classe
+    def load_sound(self, name):
+        try:
+            self.all[name] = mixer.Sound(self.get_snd_path(name))
+        except FileNotFoundError:
+            pass
+
+    # Jouer un son 
+    def play_sound(self, name):
+        if not name in self.all:
+            self.load_sound(name)
+        self.all[name].play()
 
